b039ad4c2889134a84a4fa609c4c6739701365fe
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2020-02-16 20:29:26 +1300

    Create for-loop-variables.py

---
+++ for-loop-variables.py
@@ -0,0 +1,14 @@
+''' tests a for loop with variables for start, stop and step values '''
+
+# loop starts at this number
+start_num = 4
+
+# loops stops when equals this number 
+stop_num = 20
+
+# num variable increases by this amount each time
+step = 2
+
+# loop through specified range, printing current value of num
+for num in range(start_num,stop_num,step):
+		print(num)
