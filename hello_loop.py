71650d3359fd7a6fe9a94c7c4e304c9b1e701e2e
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2020-02-16 20:33:04 +1300

    user can specify number of names to enter

--- hello_loop.py
+++ hello_loop.py
@@ -1,7 +1,11 @@
 ''' uses for loop to repeatedly ask user for name, then print Hello + name ''' 
 
-# loop 3 times
-for count in range(0,3) :
+# ask user how many names to ask for
+total_names = int(input("How many names do you wish to enter? "))
+
+
+# loop number of times specified by user
+for count in range(0,total_names) :
     name = input("Enter a name: ")
     print("Hello {}".format(name))
     
