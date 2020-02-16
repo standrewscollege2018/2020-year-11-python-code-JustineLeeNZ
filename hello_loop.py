06b08c240c839a2f0b9956eb85e9118b67f79de9
JustineLeeNZ <48225095+JustineLeeNZ@users.noreply.github.com>
2020-02-16 20:29:45 +1300

    loops fixed number of times

---
+++ hello_loop.py
@@ -0,0 +1,7 @@
+''' uses for loop to repeatedly ask user for name, then print Hello + name ''' 
+
+# loop 3 times
+for count in range(0,3) :
+    name = input("Enter a name: ")
+    print("Hello {}".format(name))
+    
